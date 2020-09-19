4b6dc3217e4b75de1c7ee4128f5e320902f561b2
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-19 15:04:10 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  render() {
+    return (
+      <div>
+      <button></button>
+      </div>
+    )
+  }
+  
+}
+
